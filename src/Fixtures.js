3d6e3d998a003a33f0f7029d6356cac2ae614a2b
rayvaldez <ray_valdez@hotmail.co.uk>
2023-01-06 11:22:01 +0000

    Input final fixture

--- src/Fixtures.js
+++ src/Fixtures.js
@@ -1,64 +1,34 @@
 const fixtures = [
   {
-      "_id": "6389265e0eab2168b945af3e",
-      "away_score": 0,
+      "_id": "6389265e0eab2168b945af40",
+      "away_score": 1,
       "away_scorers": [
           "null"
       ],
-      "away_team_id": "18",
+      "away_team_id": "17",
       "finished": "FALSE",
-      "group": "semi",
-      "home_score": 0,
+      "group": "3RD",
+      "home_score": 2,
       "home_scorers": [
           "null"
       ],
-      "home_team_id": "9",
-      "id": "60",
-      "local_date": "12/13/2022 22:00",
-      "matchday": "20",
-      "persian_date": "1400-09-22 22:30",
+      "home_team_id": "18",
+      "id": "62",
+      "local_date": "12/17/2022 22:00",
+      "matchday": "22",
+      "persian_date": "1400-09-26 22:30",
       "stadium_id": "1",
-      "time_elapsed": "notstarted",
-      "type": "semi",
-      "home_team_fa": "آرژانتین",
-      "away_team_fa": "کرواسی",
-      "home_team_en": "ARG",
-      "home_owner": "DJ",
-      "away_team_en": "CRO",
-      "away_owner": "Sean",
-      "home_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/125px-Flag_of_Argentina.svg.png",
-      "away_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/125px-Flag_of_Croatia.svg.png"
-  },
-  {
-    "_id": "6389265e0eab2168b945af3f",
-    "away_score": 0,
-    "away_scorers": [
-        "null"
-    ],
-    "away_team_id": "17",
-    "finished": "FALSE",
-    "group": "semi",
-    "home_score": 0,
-    "home_scorers": [
-        "null"
-    ],
-    "home_team_id": "15",
-    "id": "61",
-    "local_date": "12/14/2022 22:00",
-    "matchday": "21",
-    "persian_date": "1400-09-23 22:30",
-    "stadium_id": "1",
-    "time_elapsed": "notstarted",
-    "type": "semi",
-    "home_team_fa": "فرانسه",
-    "away_team_fa": "مراکش",
-    "home_team_en": "FRA",
-    "home_owner": "Jaybee",
-    "away_team_en": "MAR",
-    "away_owner": "Emma",
-    "home_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg/125px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg.png",
-    "away_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/125px-Flag_of_Morocco.svg.png"
-}
+      "time_elapsed": "finished",
+      "type": "3RD",
+      "home_team_fa": "کرواسی",
+      "away_team_fa": "مراکش",
+      "home_team_en": "Croatia",
+      "home_owner": "Sean",
+      "away_owner": "Emma",
+      "away_team_en": "Morocco",
+      "home_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/125px-Flag_of_Croatia.svg.png",
+      "away_flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/125px-Flag_of_Morocco.svg.png"
+  }
 ]
 
 export default fixtures;
